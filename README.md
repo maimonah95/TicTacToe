ee8b93e409bf71d532426ec51a671be09ded550c
maimonah95 <mim95s@hotmail.com>
2020-02-03 20:30:52 +0300

    Basic project

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # TicTacToe
+
+## technologies used
+### Browsers:
+ Google Chrome
+
+### HTML 
+### CSS
+### Programming Languages
+ Javascript 
+ ### Libraries
+ jQuery
+### approach taken To do the Project:
+Structured approach
+ ### wireframes:
+ A wireframe is a layout of a web page that demonstrates what interface elements will exist on key pages. 
+ ![Getting Started](XO.png)
+
+ ### user stories :
+  what is user stories :natural language description of one or more features of a software system.
+
+  user story :
+ * As a user, I should be able to start a new tic tac toe game.
+* As a user, I should be able to click on a square to add X first and then O, and so on.
+* As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next.
+* As a user, I should not be able to click the same square twice.
+* As a user, I should be shown a message when I win, lose or tie.
+* As a user, I should not be able to continue playing once I win, lose, or tie.
+* As a user, I should be able to play the game again without refreshing the page.
+* As a user, I should be able to play multiple game rounds.
+* As a user, I can Write my name.
+* As a user, I can play from  from a mobile phone.
+
+### how the Player win?
+
+
+
+ ### My  favorite functions:
+ when the player 
---
+++ Tic.js
@@ -0,0 +1,220 @@
+console.log('TEST TEST!!');
+
+//This object to create the players
+var players = [{
+        name: '',
+        Counter: 0
+    },
+    {
+        name: '',
+        Counter: 0
+    }
+];
+
+let count = 0; //Count the movement
+let switching = 1; //this to switch the player 
+
+$('.boardCell').on('click', function () {
+
+         Pname();
+
+    $('#right').text(players[0].name + ' : ' + players[0].Counter);
+    $('#left').text(players[1].name + ' : ' + players[1].Counter);
+
+     //check if the Cell already written  
+        if ($(this).text() !== '') {
+         console.log('this cell taken');
+         return;
+     } else {
+          //player 1 turn or player 2 ??
+        if (switching == 1) {
+            xGame()
+              //show a message 
+               $('h1').text(players[1].name + ' Turn ');
+               //change  css
+               $(this).css({
+                   'color': '#5e5e5e ',
+                   'font-size': '100px'
+               });
+               // put it inside the Cell in the game board
+               $(this).text('X');
+               checkWin('X', players[0]);
+              
+                 count++;
+                 //change to player 2
+                 switching = 0;
+                  } else {
+                      oGame();
+                 //show a message 
+                $('h1').text(players[0].name + ' Turn ');
+                 $(this).css({
+                     'color': 'seashell ',
+                     'font-size': '80px'
+                 });
+                  // put it inside the Cell in the game board
+                  $(this).text('O');
+                  checkWin('O', players[1]);
+
+                  count++;
+                  //change to player 1
+                  switching = 1;
+                  }
+                }
+             $(this).hover(function () {
+                if ($(this).text() === 'X') {
+                    $(this).css("background-color", "#FFFAF0");
+                } else {
+                    $(this).css("background-color", "#808080"); //"#D3D3D3"
+                    }
+                });
+                if (count >= 9) {
+                    console.log("woooo");
+                    $('h1').text(players[0].name + "&" + players[1].name + " : Tie");
+                    tieGame();
+                
+                    init();
+                }
+            });
+    
+    //function check if win 
+    function checkWin(playChar, nameOfPlayer) {
+            //winning posibility 
+            //   0 | 1 | 2
+            //   3 | 4 | 5
+            //   6 | 7 | 8
+            //[0, 1, 2],[3, 4, 5],[6, 7, 8], //horizontal 
+            // [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertical
+            // [0, 4, 8], [2, 4, 6]]; //diagonal 
+            //Take the value from boardCell 
+             let checkValue0 = $('#cell-0').text();
+             let checkValue1 = $('#cell-1').text();
+             let checkValue2 = $('#cell-2').text();
+             let checkValue3 = $('#cell-3').text();
+             let checkValue4 = $('#cell-4').text();
+             let checkValue5 = $('#cell-5').text();
+             let checkValue6 = $('#cell-6').text();
+             let checkValue7 = $('#cell-7').text();
+             let checkValue8 = $('#cell-8').text();
+
+             //1. check posibility
+             //2.if win -> 
+             //1. show msg , 
+             //2.increase counter of the plyer (why? if he/she diside to playe multible game ),
+             //3.display msg in counter box(Each player has a box).
+             //4. clear board game 
+             //why we dont call init()function? we want keep track with players counter
+         if (checkValue0 === playChar && checkValue1 === playChar && checkValue2 === playChar) {
+             winGame();
+             $('h1').text(nameOfPlayer.name + " : win");
+             nameOfPlayer.Counter = nameOfPlayer.Counter + 1;
+             $('#right').text(players[0].name + ' : ' + players[0].Counter);
+             $('#left').text(players[1].name + ' : ' + players[1].Counter);
+             $(".boardCell").text('');
+    }else if (checkValue3 === playChar && checkValue4 === playChar && checkValue5 === playChar) {
+             winGame();
+             $('h1').text(nameOfPlayer.name + " : win");
+             nameOfPlayer.Counter = nameOfPlayer.Counter + 1;
+             $('#right').text(players[0].name + ' : ' + players[0].Counter);
+             $('#left').text(players[1].name + ' : ' + players[1].Counter);
+             $(".boardCell").text('');
+
+    } else if (checkValue6 === playChar && checkValue7 === playChar && checkValue8 === playChar) {
+             winGame();
+             $('h1').text(nameOfPlayer.name + " : win");
+             nameOfPlayer.Counter = nameOfPlayer.Counter + 1;
+             $('#right').text(players[0].name + ' : ' + players[0].Counter);
+             $('#left').text(players[1].name + ' : ' + players[1].Counter);
+             $(".boardCell").text('');
+    } else if (checkValue0 === playChar && checkValue3 === playChar && checkValue6 === playChar) {
+             winGame();
+             $('h1').text(nameOfPlayer.name + " : win");
+             nameOfPlayer.Counter = nameOfPlayer.Counter + 1;
+             $('#right').text(players[0].name + ' : ' + players[0].Counter);
+             $('#left').text(players[1].name + ' : ' + players[1].Counter);
+             $(".boardCell").text('');
+    } else if (checkValue1 === playChar && checkValue4 === playChar && checkValue7 === playChar) {
+             winGame();
+             $('h1').text(nameOfPlayer.name + " : win");
+             nameOfPlayer.Counter = nameOfPlayer.Counter + 1;
+             $('#right').text(players[0].name + ' : ' + players[0].Counter);
+             $('#left').text(players[1].name + ' : ' + players[1].Counter);
+             $(".boardCell").text('');
+    } else if (checkValue2 === playChar && checkValue5 === playChar && checkValue8 === playChar) {
+             winGame();
+             $('h1').text(nameOfPlayer.name + " : win");
+             nameOfPlayer.Counter = nameOfPlayer.Counter + 1;
+             $('#right').text(players[0].name + ' : ' + players[0].Counter);
+             $('#left').text(players[1].name + ' : ' + players[1].Counter);
+             $(".boardCell").text('');
+    } else if (checkValue0 === playChar && checkValue4 === playChar && checkValue8 === playChar) {
+             winGame();
+             $('h1').text(nameOfPlayer.name + " : win");
+             nameOfPlayer.Counter = nameOfPlayer.Counter + 1;
+             $('#right').text(players[0].name + ' : ' + players[0].Counter);
+             $('#left').text(players[1].name + ' : ' + players[1].Counter);
+             $(".boardCell").text('');
+    } else if (checkValue2 === playChar && checkValue4 === playChar && checkValue6 === playChar) {
+             winGame();
+             $('h1').text(nameOfPlayer.name + " : win");
+             nameOfPlayer.Counter = nameOfPlayer.Counter + 1;
+             $('#right').text(players[0].name + ' : ' + players[0].Counter);
+             $('#left').text(players[1].name + ' : ' + players[1].Counter);
+             $(".boardCell").text('');
+    } else {
+        console.log("lose");
+    }
+}
+
+//this function ,When click Reset button , Reset the board(start new game).
+function init() {
+    count = 0;
+    players[0].Counter = 0;
+    players[1].Counter = 0;
+    console.log('count  :' + count);
+    console.log('count 1 :' + players[0].Counter);
+    console.log('count 2 :' + players[1].Counter);
+    $('#right').text(players[0].name + ' : ' + players[0].Counter);
+    $('#left').text(players[1].name + ' : ' + players[1].Counter);
+    return $(".boardCell").text('');
+
+}
+$('#Reset').on('click', init);
+
+// players name
+
+  function Pname() {
+    let play1 = $('#play1').val(); // player 1 
+    let play2 = $('#play2').val(); // player 2
+
+    players[0].name = play1;
+    players[1].name = play2;
+
+    if (play1 === '') {
+        players[0].name = 'X';
+    }
+    if (play2 === '') {
+        players[1].name = 'O';
+    }
+}
+ // sounds
+        function startGame() {
+          $('audio#startGame')[0].play()
+        };
+
+         function winGame() {
+           $('audio#Win')[0].play()
+        };
+
+         function xGame() {
+           $('audio#xmov')[0].play()
+        };
+
+        function oGame() {
+           $('audio#omov')[0].play()
+        };
+
+        function tieGame() {
+           $('audio#tie')[0].play()
+        };
+
+
